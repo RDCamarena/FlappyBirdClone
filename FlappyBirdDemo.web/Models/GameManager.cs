15075d504b286f3119a30ec39426614f36968343
Ruben <ruben.c05@hotmail.com>
2022-07-10 11:42:34 -0400

    First Commit

---
+++ FlappyBirdDemo.web/Models/GameManager.cs
@@ -0,0 +1,111 @@
+﻿
+
+namespace FlappyBirdDemo.web.Models
+{
+    public class GameManager 
+    {
+        private readonly int _gravity = 2;
+
+        public event EventHandler MainLoopCompleated;
+        
+
+        public BirdModel Bird { get; private set; }
+        public List<PipeModel> Pipes { get; private set; } 
+        public bool IsRunning { get; private set; } = false;
+
+
+        public GameManager()
+        {
+            Bird = new BirdModel();
+            Pipes = new List<PipeModel>();
+        }
+
+        public async void MainLoop()
+        {   
+            IsRunning = true;
+            while (IsRunning)
+            {
+
+                MoveObjs();
+
+                CheckForCollisons();
+
+                ManagePipes();
+
+
+                MainLoopCompleated?.Invoke(this, EventArgs.Empty);
+
+                await Task.Delay(20);
+
+            }
+        }
+
+
+        public void StartGame()
+        {
+            if (!IsRunning)
+            {
+                Bird = new BirdModel();
+                Pipes = new List<PipeModel>();
+                MainLoop();
+            }
+            
+        }
+
+
+        public void Jump()
+        {
+            if (IsRunning)
+            {
+                Bird.Jump();
+            }
+        }
+
+        void ManagePipes()
+        {
+            if (!Pipes.Any() || Pipes.Last().DistanceFromLeft <= 250)
+            {
+                Pipes.Add(new PipeModel());
+            }
+            if (Pipes.First().IsOffScreen())
+            {
+                Pipes.Remove(Pipes.First());
+            }
+        }
+
+        void CheckForCollisons()
+        {
+            if (Bird.IsOnGround())
+            {
+                GameOver();
+
+            }
+
+            var centeredPipe = Pipes.FirstOrDefault(p => p.IsCentered());
+
+            if (centeredPipe != null)
+            {
+                bool hasCollidedWithBottom = Bird.DistanceFromGround < centeredPipe.GapBottom - 150;
+                bool hasCollidedWithTop = Bird.DistanceFromGround + 45 > centeredPipe.GapTop - 150;
+                if(hasCollidedWithBottom || hasCollidedWithTop)
+                {
+                    GameOver();
+                }
+            }
+        }
+        void MoveObjs()
+        {
+            Bird.Fall(_gravity);
+
+
+            foreach (var pipe in Pipes)
+            {
+                pipe.Move();
+            }
+        }
+        public void GameOver()
+        {
+            IsRunning = false;
+        }
+    }
+}
---
+++ FlappyBirdDemo.web/Models/PipeModel.cs
@@ -0,0 +1,37 @@
+﻿namespace FlappyBirdDemo.web.Models
+{
+    public class PipeModel
+    {  
+        public int DistanceFromLeft { get; private set; } = 500;
+        public int DistanceFromBottom { get; private set; } = new Random().Next(60);
+        public int Speed { get; private set; } = 2;
+        public int Gap { get; private set; } = 130;
+
+        public int GapBottom => DistanceFromBottom + 300;
+        public int GapTop => GapBottom + Gap;
+
+
+        public void Move()
+        {
+            Console.WriteLine(DistanceFromBottom);
+            DistanceFromLeft -= Speed;
+        }
+
+        public bool IsOffScreen()
+        {
+            return DistanceFromLeft <= -60;
+        }
+
+        public bool IsCentered()
+        {
+            bool hasEnteredCenter = DistanceFromLeft <= (500 / 2) + (60 / 2);
+            bool hasExitCenter = DistanceFromLeft <= (500 / 2) - (60 / 2) - 60;
+
+
+            return hasEnteredCenter && !hasExitCenter;
+        }
+    }
+
+}
+
+
